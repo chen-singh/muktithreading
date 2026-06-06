a9055ac889dba0025d6ee41ef3323717603ec8c9
chen-singh <cssodho123@gmail.com>
2026-06-06 22:35:51 +0400

    add files

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@ public class Test implements Runnable{
         for (int i=0;i<100000;i++){
             System.out.println("chen");
         }
+
+        System.out.println("hello chen how are you");
     }
 }
