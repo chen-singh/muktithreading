26613757e6bb550ce5db08c4381902213dbe87f4
chen-singh <cssodho123@gmail.com>
2026-06-06 19:27:57 +0400

    add files

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Test test=new Test();
+        test.run();
+        for (int i=0;i<100000;i++){
+            System.out.println("hello ");
+        }
+        System.out.println(Thread.currentThread().getName());
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test implements Runnable{
+    @Override
+    public void run() {
+        for (int i=0;i<100000;i++){
+            System.out.println("chen");
+        }
+    }
+}
